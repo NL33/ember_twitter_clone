3fc316cb457f962214df764b2de773846c6d3a14
NL33 <stkellman@gmail.com>
2015-03-11 17:32:30 -0400

    add ability to add chirps

---
+++ controllers/chirps.js
@@ -0,0 +1,17 @@
+Chirper.ChirpsController = Ember.ArrayController.extend({  //here, the “ObjectController.extend”
+  sortProperties: ["date"],
+  isAddingChirp: false,
+  actions: {
+    add_new_chirp: function() {
+      this.set('isAddingChirp', true);
+    },
+    save_chirp: function() {
+      chirps.addObject({id: (chirps.length + 1).toString(), 
+      text: this.text, 
+      user_id: this.user_id,
+      date: new Date().toLocaleString()
+      })
+      this.set('isAddingChirp', false);
+    }
+  }
+});
